18d04a13298ad89bedaa237e9afc0cecd880ce01
Charlie Anderson <charlander@google.com>
2025-08-26 14:18:51 -0400

    Add methods for getting default shape bitmap
    Flag: com.android.launcher3.enable_launcher_icon_shapes
    Test: restoring in downgrade and upgrade scenarios
    Bug: 439974991
    
    Change-Id: I5d76985938a1624b003450790ff6df15e43abf69

--- iconloaderlib/src/com/android/launcher3/icons/GraphicsUtils.kt
+++ iconloaderlib/src/com/android/launcher3/icons/GraphicsUtils.kt
@@ -30,6 +30,8 @@ import android.graphics.ColorMatrixColorFilter
 import android.graphics.Matrix
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.graphics.Rect
 import android.graphics.RectF
 import android.util.Log
@@ -73,6 +75,28 @@ object GraphicsUtils {
             Log.w(TAG, "Could not write bitmap")
             return ByteArray(0)
         }
+    }
+
+    /** Compresses BitmapInfo default shape bitmap to a byte array **/
+    @JvmStatic
+    fun createDefaultFlatBitmap(bitmapInfo: BitmapInfo): ByteArray {
+        val bitmap = bitmapInfo.icon.copy(Bitmap.Config.ARGB_8888, /* isMutable **/ true)
+        val cropBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(cropBitmap)
+
+        var paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.FILL
+        canvas.drawPath(bitmapInfo.defaultIconShape.path, paint)
+
+        paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
+        paint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
+        canvas.drawBitmap(bitmap, 0f, 0f, paint)
+
+        val flatBitmap = flattenBitmap(cropBitmap)
+        cropBitmap.recycle()
+        bitmap.recycle()
+        return flatBitmap
     }
 
     /** Tries to decode the [ByteArray] into a [Bitmap] consuming any parsing errors */
